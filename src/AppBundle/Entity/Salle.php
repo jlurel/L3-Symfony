600ccb7347071b9de019e0d27d499a41660e4f58
younesakhrif <36633762+younesakhrif@users.noreply.github.com>
2018-02-19 16:13:02 +0100

    Add files via upload

---
+++ src/AppBundle/Entity/Salle.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Salle
+ *
+ * @ORM\Table(name="salle")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\SalleRepository")
+ */
+class Salle
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="numSalle", type="integer")
+     */
+    private $numSalle;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="disponibilite", type="boolean")
+     */
+    private $disponibilite;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set numSalle
+     *
+     * @param integer $numSalle
+     *
+     * @return Salle
+     */
+    public function setNumSalle($numSalle)
+    {
+        $this->numSalle = $numSalle;
+
+        return $this;
+    }
+
+    /**
+     * Get numSalle
+     *
+     * @return int
+     */
+    public function getNumSalle()
+    {
+        return $this->numSalle;
+    }
+
+    /**
+     * Set disponibilite
+     *
+     * @param boolean $disponibilite
+     *
+     * @return Salle
+     */
+    public function setDisponibilite($disponibilite)
+    {
+        $this->disponibilite = $disponibilite;
+
+        return $this;
+    }
+
+    /**
+     * Get disponibilite
+     *
+     * @return bool
+     */
+    public function getDisponibilite()
+    {
+        return $this->disponibilite;
+    }
+}
+
